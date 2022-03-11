1eb74f3f1b98ba6a41346b81238b140cddafd5fb
Ashan Dulanja <ashan901121@gmail.com>
2022-03-12 01:05:41 +0800

    adding 1st controller

---
+++ InsapectionAPI/InspectionType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsapectionAPI
+{
+    public class InspectionType
+    {
+        public int Id { get; set; }
+        [StringLength(20)]
+        public int InspectionName { get; set; } = string.Empty;
+    }
+}
